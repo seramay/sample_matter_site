bf85e2c999aed8d658391457aa13709101b45bce
seramay <lilina172ki@gmail.com>
2020-07-09 21:10:08 +0900

    create_companies_markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recruitments/index'
+  get 'recruitments/new'
+  get 'recruitments/create'
+  get 'recruitments/edit'
+  get 'recruitments/update'
+  get 'recruitments/destroy'
+  get 'recruitments/show'
   resources :companies, except: :index
 
   root "companies#index"
